566d40d1fd95a7c278ed25dbc36fb1f83262bfe9
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-02-16 18:58:55 +0100

    GymManager - contact form view

--- GymManager.UI/Controllers/HomeController.cs
+++ GymManager.UI/Controllers/HomeController.cs
@@ -1,7 +1,3 @@
-using System.Diagnostics;
-using GymManager.Application.Tickets.Commands.AddTicket;
-using GymManager.Application.Tickets.Queries.GetTicketById;
-using GymManager.UI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManager.UI.Controllers;
@@ -13,29 +9,49 @@ public class HomeController : BaseController
 {
 	private readonly ILogger _logger;
 
+	// konstruktor - dependency injection - wstrzyknięcie klasy NLoga
 	public HomeController(ILogger<HomeController> logger)
 	{
 		_logger = logger;
 	}
 
-	public async Task<IActionResult> Index()
+	// widok strona główna
+	public IActionResult Index()
 	{
-		// symulacja błędu
-		//throw new Exception("Nieobsłużony błąd!");
-
-		// INFO - wywołanie kwerendy
-		// parametr to nazwa kwerendy, czyli klasa Query
-		//var ticket = await Mediator.Send(new GetTicketByIdQuery { TicketId = 1 });
-
-		// INFO - wywołanie kwerendy
-		await Mediator.Send(new AddTicketCommand { Name = "Ticket1" });
-
-		// INFO - użycie NLog
-		_logger.LogInformation("LogInformation");
-		_logger.LogError(new Exception("LogError"), null);
-
 		return View();
 	}
+
+	// widok polityka prywatności
+	public IActionResult Privacy()
+	{
+		return View();
+	}
+
+	// widok kontakt
+	public IActionResult Contact()
+	{
+		return View();
+	}
+
+	// testowa akcja Index
+	//public async Task<IActionResult> Index()
+	//{
+	//	// symulacja błędu
+	//	//throw new Exception("Nieobsłużony błąd!");
+
+	//	// INFO - wywołanie kwerendy
+	//	// parametr to nazwa kwerendy, czyli klasa Query
+	//	//var ticket = await Mediator.Send(new GetTicketByIdQuery { TicketId = 1 });
+
+	//	// INFO - wywołanie kwerendy
+	//	//await Mediator.Send(new AddTicketCommand { Name = "Ticket1" });
+
+	//	// INFO - użycie NLog
+	//	//_logger.LogInformation("LogInformation");
+	//	//_logger.LogError(new Exception("LogError"), null);
+
+	//	return View();
+	//}
 
 	// INFO - routing - definiowanie ścieżki nad konkretną akcją
 
@@ -63,19 +79,15 @@ public class HomeController : BaseController
 	// to adres nie musi zawierać całej ścieżki i nie łączy tych adresów
 	// przykład: kontroler - [Route("test/[controller]/[action]")], akcja - [HttpGet("/[controller]/[action]/{test}")]
 	// efekt: www.adres.pl/test/home/index2/wartosc
-	public IActionResult Index2(string test)
-	{
-		return View("Index");
-	}
+	//public IActionResult Index2(string test)
+	//{
+	//	return View("Index");
+	//}
 
-	public IActionResult Privacy()
-	{
-		return View();
-	}
-
-	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-	public IActionResult Error()
-	{
-		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-	}
+	// to jest nie potrzebne bo jest własna obsługa błędów
+	//[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	//public IActionResult Error()
+	//{
+	//	return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	//}
 }
