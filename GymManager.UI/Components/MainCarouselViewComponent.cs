d66df1d1e90c4db97afb01cf6fa66cdac76cd0b1
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-02-17 16:37:18 +0100

    GymManager - components

---
+++ GymManager.UI/Components/MainCarouselViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.UI.Components;
+
+// INFO - komponent
+public class MainCarouselViewComponent : ViewComponent
+{
+	public async Task<IViewComponentResult> InvokeAsync(string priority)
+	{
+		// jakaś logika
+		// pobranie z bazy danych
+		int timeInterval = 1500;
+		return View(timeInterval);
+	}
+}
