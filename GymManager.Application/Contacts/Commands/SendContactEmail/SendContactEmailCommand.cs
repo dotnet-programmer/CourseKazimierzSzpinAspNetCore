91d9147d53cc58b26ac7b38b764d29a790a9aad1
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-02-17 10:01:41 +0100

    GymManager - data validation by DataAnnotation

--- GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommand.cs
+++ GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommand.cs
@@ -1,12 +1,22 @@
-﻿using MediatR;
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
 
 namespace GymManager.Application.Contacts.Commands.SendContactEmail;
 
 // model dla formularza w widoku Contact.cshtml
 public class SendContactEmailCommand : IRequest
 {
+	// walidacja danych za pomocą DataAnnotation
+	[Required(ErrorMessage = "Pole 'Imię i Nazwisko' jest wymagane.")]
 	public string Name { get; set; }
+
+	[Required(ErrorMessage = "Pole 'Adres e-mail' jest wymagane.")]
+	[EmailAddress(ErrorMessage = "Pole 'Adres e-mail' nie jest prawidłowym adresem e-mail")]
 	public string Email { get; set; }
+
+	[Required(ErrorMessage = "Pole 'Tytuł wiadomości' jest wymagane.")]
 	public string Title { get; set; }
+
+	[Required(ErrorMessage = "Pole 'Wiadomość' jest wymagane.")]
 	public string Message { get; set; }
 }
--- GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommandValidator.cs
+++ GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommandValidator.cs
@@ -6,9 +6,12 @@ namespace GymManager.Application.Contacts.Commands.SendContactEmail;
 // wskazanie, że tutaj będą zasady walidacyjne do pól z komendy SendContactEmailCommand
 public class SendContactEmailCommandValidator : AbstractValidator<SendContactEmailCommand>
 {
-	// w konstruktorze tworzy się różne zasady dla różnych pól
-	public SendContactEmailCommandValidator() =>
-		RuleFor(x => x.Name)
-			.NotEmpty()
-			.WithMessage("Pole 'Imię i Nazwisko' jest wymagane.");
+	// wzięte w komentarz, bo stosowana inna walidacja - DataAnnotation
+	//// w konstruktorze tworzy się różne zasady dla różnych pól
+	//public SendContactEmailCommandValidator()
+	//{
+	//	RuleFor(x => x.Name)
+	//		.NotEmpty()
+	//		.WithMessage("Pole 'Imię i Nazwisko' jest wymagane.");
+	//}
 }
