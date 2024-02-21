dc0aa836919fabd1f65d3bd0797f2fa97917cfc1
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-02-21 20:12:40 +0100

    GymManager - pagination of data in a table

---
+++ GymManager.Application/Common/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using GymManager.Application.Common.Models;
+
+namespace GymManager.Application.Common.Extensions;
+
+// rozszerzenie ułatwiające wywołanie klasy PaginatedList
+public static class IQueryableExtensions
+{
+	// metoda ułatwiająca zamienianie listy IQueryable na odpowiednia stronę w tabeli (paginacja danych)
+	public static async Task<PaginatedList<T>> PaginatedListAsync<T>(this IQueryable<T> queryable, int pageNumber, int pageSize) =>
+		await PaginatedList<T>.CreateAsync(queryable, pageNumber, pageSize);
+}
---
+++ GymManager.Application/Common/Models/PaginatedList.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GymManager.Application.Common.Models;
+
+// klasa generyczna, używana dla dowolnych typów gdy będzie potrzebne stronicowanie w konkretnych kwerendach
+public class PaginatedList<T>
+{
+	public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+	{
+		PageIndex = pageIndex;
+		TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+		TotalCount = count;
+		Items = items;
+	}
+
+	// lista generyczna z elementami listy
+	public List<T> Items { get; }
+	public int PageIndex { get; }
+	public int TotalPages { get; }
+	public int TotalCount { get; }
+	public bool HasPreviousPage => PageIndex > 1;
+	public bool HasNextPage => PageIndex < TotalPages;
+
+	// metoda statyczna która będzie tworzyła tą listę
+	public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+	{
+		var count = await source.CountAsync();
+
+		// pobieranie konkretnych danych z odpowiedniej strony
+		var items = await source
+			.Skip((pageIndex - 1) * pageSize)
+			.Take(pageSize)
+			.ToListAsync();
+
+		return new PaginatedList<T>(items, count, pageIndex, pageSize);
+	}
+}
