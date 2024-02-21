5a07bad5fdbdab59088c839d079e0a03e6a59a5b
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-02-21 19:37:12 +0100

    GymManager - display tickets - view - advanced tables rendered on the server side

---
+++ GymManager.UI/Controllers/TicketController.cs
@@ -0,0 +1,17 @@
+﻿using GymManager.Application.Clients.Queries.GetClient;
+using GymManager.Application.Tickets.Commands.AddTicket;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.UI.Controllers;
+
+[Authorize]
+public class TicketController : BaseController
+{
+	public async Task<IActionResult> Tickets()
+	{
+		var isUserDataCompleted = !string.IsNullOrWhiteSpace((await Mediator.Send(new GetClientQuery { UserId = UserId })).FirstName);
+
+		return View(isUserDataCompleted);
+	}
+}
