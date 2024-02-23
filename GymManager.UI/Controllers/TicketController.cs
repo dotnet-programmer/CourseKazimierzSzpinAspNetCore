17a3e57679f9d9542af97af5d615051361d46f3d
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-02-23 14:38:00 +0100

    GymManager - print ticket

--- GymManager.UI/Controllers/TicketController.cs
+++ GymManager.UI/Controllers/TicketController.cs
@@ -117,4 +117,15 @@ public class TicketController : BaseController
 
 		return File(TempData.Get<byte[]>(fileGuid), "application/pdf", fileName);
 	}
+
+	public async Task<IActionResult> PrintTicket(string id)
+	{
+		var ticket = await Mediator.Send(new GetPrintTicketQuery
+		{
+			TicketId = id,
+			UserId = UserId
+		});
+
+		return View("TicketPreview", ticket);
+	}
 }
