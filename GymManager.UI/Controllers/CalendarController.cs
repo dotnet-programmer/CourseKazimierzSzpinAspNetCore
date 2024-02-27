34f6873a058f33d66412f724f049a6d9648fc486
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-02-27 19:51:18 +0100

    GymManager - advanced modal window

--- GymManager.UI/Controllers/CalendarController.cs
+++ GymManager.UI/Controllers/CalendarController.cs
@@ -1,5 +1,6 @@
 ﻿using GymManager.Application.Dictionaries;
 using GymManager.Application.EmployeeEvents.Queries.GetEmployeeEvents;
+using GymManager.Application.Employees.Queries.GetEmployeeBasics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,8 +9,8 @@ namespace GymManager.UI.Controllers;
 [Authorize(Roles = $"{RolesDict.Administrator},{RolesDict.Pracownik}")]
 public class CalendarController : BaseController
 {
-	public IActionResult Calendar() =>
-		View();
+	public async Task<IActionResult> Calendar() =>
+		View(await Mediator.Send(new GetEmployeeBasicsQuery()));
 
 	// pobranie informacji o wszystkich zdarzeniach pracowników, używana w widoku generowania całego kalendarza
 	public async Task<IActionResult> GetEmployeeEvents() 
