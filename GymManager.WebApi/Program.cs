8b6e7db315e5cfb91c8e0edffa06971844df67b5
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-02-29 19:27:50 +0100

    GymManager - WebApi configuration

--- GymManager.WebApi/Program.cs
+++ GymManager.WebApi/Program.cs
@@ -1,13 +1,32 @@
+using GymManager.Application;
+using GymManager.Application.Common.Interfaces;
+using GymManager.Infrastructure;
+using Microsoft.Extensions.Options;
+using static QRCoder.PayloadGenerator;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// dodanie Dependency Injection z innych projektów
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
+
+// dodanie Dependency Injection z innych projektów
+using (var scope = app.Services.CreateScope())
+{
+	app.UseInfrastructure(
+		scope.ServiceProvider.GetRequiredService<IApplicationDbContext>(),
+		app.Services.GetService<IAppSettingsService>(),
+		app.Services.GetService<IEmailService>(),
+		app.Services.GetService<IWebHostEnvironment>());
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
